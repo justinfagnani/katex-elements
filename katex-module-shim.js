3049382fa833f3a2ee97aee7be329300690cdb50
Justin Fagnani <justinfagnani@google.com>
2018-05-11 12:59:34 -0700

    Fix: change exports to module

--- katex-module-shim.js
+++ katex-module-shim.js
@@ -1,4 +1,4 @@
 export const originalExports = window.exports;
-export const originalModule = window.exports;
+export const originalModule = window.module;
 export const katex = window.exports = {};
 window.module = {exports};
